6cccd3923945996ebd67f543bac4e3d8f3b575b7
Saitejas Mopuri <saitejasmopuri@knights.ucf.edu>
2023-12-10 22:31:21 -0500

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,22 @@
+// function getSystemIRecommendations() {
+//     var genre = document.getElementById('genre').value;
+//     console.log("I am here")
+//     $.ajax({
+//         type: 'GET',
+//         url: '/system_i_recommendations',
+//         data: { genre: genre },
+//         success: function (data) {
+//             document.getElementById('system-i-results').innerHTML = data;
+//         }
+//     });
+// }
+
+// function rateMovies() {
+//     $.ajax({
+//         type: 'GET',
+//         url: '/system_ii_rate_movies',
+//         success: function (data) {
+//             document.getElementById('movies-to-rate').innerHTML = data;
+//         }
+//     });
+// }
